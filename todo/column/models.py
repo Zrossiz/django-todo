0bc44aa1a9b1379cc7d8ce2b9dda48c8417f7ce0
Zrossiz <soon.pluxury@mail.ru>
2024-04-19 16:51:09 +0300

    feat: added task and column model

--- todo/column/models.py
+++ todo/column/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Column(models.Model):
+    name = models.CharField(max_length=255)
+    color = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'Column'
--- todo/task/models.py
+++ todo/task/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    priority = models.IntegerField()
+    column = models.ForeignKey(to='column.Column', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'Task'
